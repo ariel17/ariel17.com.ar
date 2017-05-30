58ef19949653ef0b8042259d283ccfa699c99cb9
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2017-05-30 12:31:25 -0300

    Adds more basic content to page site.

--- site2/gulpfile.js
+++ site2/gulpfile.js
@@ -1,14 +1,44 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var minify = require('gulp-minify');
 var browserSync = require('browser-sync').create();
 
+var files = {
+    less: [
+      './bower_components/bootstrap-less/less/bootstrap.less',
+      './assets/less/**/*.less'
+    ],
+    html: [
+        'index.html'
+    ],
+    javascript: [
+        './bower_components/jquery/dist/jquery.js',
+        './bower_components/bootstrap-less/js/collapse.js'
+    ]
+};
+
+
 gulp.task('less', function () {
-  return gulp.src('./assets/less/**/*.less')
+  return gulp.src(files.less)
     .pipe(less())
     .pipe(gulp.dest('./public/css'));
 });
 
+gulp.task('js', function() {
+  gulp.src(files.javascript)
+    .pipe(minify({
+        ext:{
+            src:'.js',
+            min:'.min.js'
+        },
+        ignoreFiles: ['min.js'],
+        noSource: true
+    }))
+    .pipe(gulp.dest('public/js'));
+});
+
 gulp.task('sync', function() {
+    gulp.watch(files.less, ['less']);
     browserSync.init({
         server: {
             baseDir: "./"
@@ -16,6 +46,4 @@ gulp.task('sync', function() {
     });
 });
 
-gulp.watch('./assets/less/**/*.less', ['less']);
-
-gulp.task('default', ['less', 'sync']);
+gulp.task('default', ['less', 'js', 'sync']);
